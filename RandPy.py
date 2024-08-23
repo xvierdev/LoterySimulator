a98db7c4c2d92324faf860f725162e80b7dc9af0
Weley Xavier <wesley.xvier@gmail.com>
2024-08-23 18:52:11 -0300

    Update no código 2

--- RandPy.py
+++ RandPy.py
@@ -2,6 +2,10 @@ import random
 # Teste de números aleatórios
 # Este programa sorteia uma sequencia de números como no Jogo da Mega Sena e determina quantas jogadas foi necessário até o número ser sorteado e o valor gasto em reais
 
+sequence = []
+for i in range (0,6):
+    sequence.append(input(f'Digite o{i}° número entre 0 e 60 sem repetição: '))
+    
 cont = 0
 while True:
     sequence = random.sample(range(1,61), 6)
@@ -11,3 +15,4 @@ while True:
     if sequence == [2, 3, 5, 15, 21, 39]:
         break
 print(f'Ganhou na Mega Sena com {cont} jogadas! Gastou R$ {3.5*cont:.2f}')
+input('Press enter to quit ')
